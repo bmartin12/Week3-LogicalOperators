214f421d565b2eaacbacb120f35168549b08672b
Opilane TTHK <opilane@tthk.ee>
2021-09-14 12:05:26 +0300

    logic operators

---
+++ BooleanLogicOperators/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+namespace BooleanLogicOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada kasutajatunnus;
+            //programm küsib kasutajal sisestada parool;
+            //programm kontrollib, kas mõlemad, nii kasutajatunnus
+            //kui ka parool on õiged;
+            //kui mõlemad, kasutajatunnus ja parool on õiged
+            //programm kuvab konsoolis "tere tulemast!"
+            //kasutajatunnus või parool on vale, siis
+            //programm kuvab sisselogimine ebaõnnestus. Proovi uuesti."
+            //kt: admin, pr:admin1234
+
+            Console.WriteLine("Enter your username");
+            string username = Console.ReadLine();
+            Console.WriteLine("Enter your password");
+            string password = Console.ReadLine();
+
+            if (username == "swaggin1" && password == "applejack")
+            {
+                Console.WriteLine("welcome back, sir ;)");
+            }
+            else
+            {
+                Console.WriteLine("uh oh seems like yer username or password is wrong :(");
+            }
+
+
+
+        }
+    }
+}
---
+++ Login/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your username");
+            string username = Console.ReadLine();
+            Console.WriteLine("Enter your password");
+            string password = Console.ReadLine();
+
+            if (username != "swaggin1" || password != "applejack")
+            {
+                Console.WriteLine("welcome back, sir ;)");
+            }
+            else
+            {
+                Console.WriteLine("uh oh seems like yer username or password is wrong :(");
+            }
+        }
+    }
+}
